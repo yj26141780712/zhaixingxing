4bfa298779332911775e9a09abb38cf54256685f
yangjie <261417807@qq.com>
2022-01-25 13:17:26 +0800

    增加comboBox组件

---
+++ assets/component/comboBox/comboBox.ts
@@ -0,0 +1,111 @@
+
+import { _decorator, Component, Node, Label, Prefab, instantiate, UITransform, tween, Quat, Vec3 } from 'cc';
+import { item } from './item';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = comboBox
+ * DateTime = Tue Jan 25 2022 10:48:21 GMT+0800 (中国标准时间)
+ * Author = yj261417807
+ * FileBasename = comboBox.ts
+ * FileBasenameNoExtension = comboBox
+ * URL = db://assets/scripts/comboBox.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('comboBox')
+export class comboBox extends Component {
+
+    @property(Node)
+    public triangle: Node | null = null;
+
+    @property(Label)
+    public comboLabel: Label | null = null;
+
+    @property(Node)
+    public dropdown: Node | null = null;
+
+    @property(Node)
+    public vLayout: Node | null = null;
+
+    @property(Node)
+    public content: Node | null = null;
+
+    @property(Prefab)
+    public itemPrefab: Prefab | null = null;
+
+
+    isDropdown = false;
+    itemArray = ['Cocos Creator', 'Cocos-2dx', 'Cocos2d-js', 'Cocos2d-Lua', 'Cocos Creator 3D', 'Cocos Service', 'Cocos社区'];
+
+    start() {
+        console.log(456);
+        this.dropdown.active = false;
+        this.initItems();
+        // [3]
+    }
+
+    initItems() {
+        let totalHeight = 0;
+        this.itemArray.forEach((x) => {
+            let item = instantiate(this.itemPrefab);
+            item.children[0].getComponent(Label).string = x;
+            this.vLayout.addChild(item);
+            const component = <item>item.getComponent('item');
+            component.comboBox = this;
+            const ui = item.getComponent(UITransform);
+            totalHeight += ui.height;
+        });
+        const uiContent = this.content.getComponent(UITransform);
+        if (totalHeight > uiContent.height) {
+            uiContent.height = totalHeight;
+        }
+    }
+
+    onClick() {
+        this.isDropdown = !this.isDropdown;
+        this.showHideDropDownBox();
+        this.rotateTriangle();
+    }
+
+    rotateTriangle() {
+        if (this.isDropdown) {
+            const quat = new Quat();
+            Quat.fromEuler(quat, 0, 0, 180);
+            tween(this.triangle).to(0.5, {
+                rotation: quat
+            }, { easing: "cubicOut" }).start()
+        } else {
+            const quat = new Quat();
+            Quat.fromEuler(quat, 0, 0, 90);
+            tween(this.triangle).to(0.5, {
+                rotation: quat
+            }, { easing: "cubicOut" }).start()
+        }
+    }
+
+    showHideDropDownBox() {
+        if (this.isDropdown) {
+            this.dropdown.active = true;
+        } else {
+            this.dropdown.active = false;
+        }
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/zh/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/zh/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/zh/scripting/life-cycle-callbacks.html
+ */
---
+++ assets/scripts/bgMove.ts
@@ -0,0 +1,77 @@
+
+import { _decorator, Component, Node, Animation, animation, AnimationClip, UITransform } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = bgMove
+ * DateTime = Tue Jan 25 2022 08:53:32 GMT+0800 (中国标准时间)
+ * Author = yj261417807
+ * FileBasename = bgMove.ts
+ * FileBasenameNoExtension = bgMove
+ * URL = db://assets/scripts/bgMove.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+
+@ccclass('bgMove')
+export class bgMove extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    // @property
+    // serializableDummy = 0;
+    @property()
+    public speed = 0;
+
+    @property(Animation)
+    public snailAnimation: Animation | null = null;
+
+    @property(Node)
+    public bg1Node: Node | null = null;
+
+    @property(Node)
+    public bg2Node: Node | null = null;
+
+    triggerX = 0;
+
+    start() {
+        // [3]
+        this.snailAnimation.clips[0].speed = this.speed / 50;
+        this.snailAnimation.clips[0].wrapMode = AnimationClip.WrapMode.Loop;
+        console.log(this.snailAnimation);
+        this.snailAnimation.play()
+        const state = this.snailAnimation.getState(this.snailAnimation.clips[0].name);
+        state.speed = this.speed / 50;
+        state.wrapMode = AnimationClip.WrapMode.Loop;
+        const ui = this.bg1Node.getComponent(UITransform);
+        this.triggerX = -ui.width;
+    }
+
+    update(deltaTime: number) {
+        const position1 = this.bg1Node.getPosition();
+        const position2 = this.bg2Node.getPosition();
+        position1.x -= this.speed * deltaTime;
+        position2.x -= this.speed * deltaTime;
+        console.log(position1, position2);
+        if (position1.x <= this.triggerX) {
+            position1.x = position2.x - this.triggerX;
+        } else if (position2.x <= this.triggerX) {
+            position2.x = position1.x - this.triggerX;
+        }
+        this.bg1Node.setPosition(position1);
+        this.bg2Node.setPosition(position2);
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/zh/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/zh/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/zh/scripting/life-cycle-callbacks.html
+ */
